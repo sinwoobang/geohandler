7bb7c0d46b9517906e117dcbbb7b53952ac19618
sinwoobang <sinwoobang@gmail.com>
2019-09-16 15:06:35 +0300

    Implement API assets

--- geohandler/apis/meta/assets.py
+++ geohandler/apis/meta/assets.py
@@ -1,8 +1,10 @@
 import http
+import geojson
 
-from flask import Blueprint
+from typing import List, Optional
+from flask import Blueprint, request
 from flask.views import MethodView
-
+from geojson import FeatureCollection, Feature, Point
 
 # Didn't use the prefix '/api/meta/assets' but '/api/meta' because some api use 'asset' not 'assets'.
 api_meta_assets_bp = Blueprint('api_meta_assets_bp', __name__, url_prefix='/api/meta')
@@ -10,7 +12,66 @@ api_meta_assets_bp = Blueprint('api_meta_assets_bp', __name__, url_prefix='/api/
 
 class MetaAssetsAPI(MethodView):
     def get(self):
-        return {'code': http.HTTPStatus.OK, 'message': 'This is GET.'}
+        raw_sources = request.args.get('sources', '', str)
+        sources: List[str] = raw_sources.split(',')
+
+        lat: Optional[float] = request.args.get('lat', type=float)
+        lon: Optional[float] = request.args.get('lon', type=float)
+        radius: Optional[float] = request.args.get('radius', type=float)
+        if not lat or not lon or not radius:
+            return {
+                'code': http.HTTPStatus.BAD_REQUEST,
+                'message': http.HTTPStatus.BAD_REQUEST.phrase,
+                'error': 'Data lat, lon, radius are required.'
+            }
+
+        raw_bbox = request.args.get('bbox', '', str)
+        try:
+            bbox: List[float] = list(map(float, raw_sources.split(',')))
+        except (TypeError, ValueError):  # Case of None
+            bbox = []
+
+        building: Optional[str] = request.args.get('building', type=str)
+        if not building:
+            return {
+                'code': http.HTTPStatus.BAD_REQUEST,
+                'message': http.HTTPStatus.BAD_REQUEST.phrase,
+                'error': 'Data building is required.'
+            }
+
+        raw_asset_tracking_ids = request.args.get('assetTrackingIds', '', str)
+        asset_tracking_ids: List[str] = raw_asset_tracking_ids.split(',')
+        if not asset_tracking_ids[0]:  # the value of [0] will be empty string if there is nothing.
+            return {
+                'code': http.HTTPStatus.BAD_REQUEST,
+                'message': http.HTTPStatus.BAD_REQUEST.phrase,
+                'error': 'Data assetTrackingIds is required.'
+            }
+
+        raw_tags_any = request.args.get('tags_any', '', str)
+        tags_any: List[str] = raw_asset_tracking_ids.split(',')
+
+        raw_tags_all = request.args.get('tags_all', '', str)
+        tags_all: List[str] = raw_asset_tracking_ids.split(',')
+
+        operator: Optional[str] = request.args.get('operator', type=str)
+        advertiser: Optional[str] = request.args.get('advertiser', type=str)
+        limit: Optional[int] = request.args.get('limit', type=int)
+
+        point = Point((lon, lat))
+        properties = {
+            'radius': radius,
+            'assetTrackingId': asset_tracking_ids[0],
+            'buildingRef': building
+        }
+        feature = Feature(id="1", geometry=point, properties=properties)
+        features = [feature]
+        feature_collection = FeatureCollection(features=features)
+        return {
+            'code': http.HTTPStatus.OK,
+            'message': http.HTTPStatus.OK.phrase,
+            'data': geojson.dumps(feature_collection)
+        }
 
     def post(self):
         return {'code': http.HTTPStatus.OK, 'message': 'This is POST.'}
--- tests/test_apis_assets.py
+++ tests/test_apis_assets.py
@@ -1,7 +1,10 @@
 import http
+from typing import Dict, Union
 
+import geojson
 import pytest
 from flask.testing import FlaskClient
+from geojson import Point
 
 from config import TestingConfig
 from geohandler import create_app
@@ -23,11 +26,63 @@ def client():
     ctx.pop()
 
 
-def test_index(client: FlaskClient):
+def test_assets(client: FlaskClient):
     """Test for /api/meta/assets"""
     url = '/api/meta/assets'
+    lat = 35.818329
+    lon = 128.251586
+    radius = 100
+    building = 'building1'
+    asset_tracking_ids = ['1']
+
+    params_type = Dict[str, Union[str, float]]
+
+    # Without any params
     res = client.get(url)
-    assert res.json['code'] == http.HTTPStatus.OK
+    assert res.json['code'] == http.HTTPStatus.BAD_REQUEST
+
+    # Without building.
+    params: params_type = {
+        'lat': lat,
+        'lon': lon,
+        'radius': radius
+    }
+    res = client.get(url, query_string=params)
+    assert res.json['code'] == http.HTTPStatus.BAD_REQUEST
+
+    # Without asset tracking ids.
+    params: params_type = {  # type: ignore
+        'lat': lat,
+        'lon': lon,
+        'radius': radius,
+        'building': building
+    }
+
+    res = client.get(url, query_string=params)
+    assert res.json['code'] == http.HTTPStatus.BAD_REQUEST
+
+    # With everything
+    params = {
+        'lat': lat,
+        'lon': lon,
+        'radius': radius,
+        'building': building,
+        'assetTrackingIds': ','.join(asset_tracking_ids)
+    }
+    res = client.get(url, query_string=params)
+    res_json = res.json
+    assert res_json['code'] == http.HTTPStatus.OK  # type: ignore
+
+    res_raw_data = res_json['data']  # geojson
+    print(res_raw_data)
+    res_data = geojson.loads(res_raw_data)
+    res_data_feature = res_data['features'][0]
+    res_data_properties = res_data_feature['properties']
+    assert res_data_feature['id'] == '1'
+    assert res_data_feature['geometry'] == Point((lon, lat))
+    assert res_data_properties['assetTrackingId'] == asset_tracking_ids[0]
+    assert res_data_properties['buildingRef'] == building
+    assert res_data_properties['radius'] == radius
 
 
 def test_assets_id(client: FlaskClient):
